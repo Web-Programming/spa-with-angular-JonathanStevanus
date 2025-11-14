1127fbc44f697fd4bfadcfea5ddef4d86db6100b
JonathanStevanus <jonathanstv1343@gmail.com>
2025-11-14 17:24:34 +0700

    mengerjakan griya-mdp

---
+++ griya-mdp/src/app/app.routes.ts
@@ -0,0 +1,37 @@
+import { Routes } from '@angular/router';
+import { Home as HomeComponent } from './home/home';
+import { Profile } from './profile/profile';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Contact } from './contact/contact';
+import { LokasiPerumahan } from './lokasi-perumahan/lokasi-perumahan';
+export const routes: Routes = [
+    //mengatur halaman utama aplikasi
+    {
+        path : "",
+        component : HomeComponent,
+        title : 'Home Page'
+    },
+    {
+        path : "profile",
+        component : Profile,
+        //title : 'Profile Page'
+    },
+    {
+        path : "login",
+        component : Login,
+    },
+    {
+        path : "register",
+        component : Register,
+    },
+    {
+        path : "contact",
+        component : Contact,
+    },
+    {
+        path : "lokasi-perumahan",
+        component : LokasiPerumahan,
+    }
+
+];
---
+++ griya-mdp/src/app/home/home.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LokasiPerumahan } from '../lokasi-perumahan/lokasi-perumahan';
+import { CommonModule } from '@angular/common';
+import { Housing } from '../lokasi-perumahan/lokasi-perumahan';
+@Component({
+  selector: 'app-home',
+  imports: [LokasiPerumahan, CommonModule, RouterLink],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  // Array untuk data perumahan (bisa diisi dari backend nanti)
+  housingList = [
+    {
+      id: 1,
+      name: 'Griya Asri Residence',
+      location: 'Jakarta Selatan',
+      price: 850000000,
+      bedrooms: 3,
+      bathrooms: 2,
+      area: 120,
+      image: 'https://images.unsplash.com/photo-1568605114967-8130f3a36994',
+      rating: 4.5,
+      status: 'Available'
+    },
+
+  ];
+
+  filteredList: Housing[] = [];
+
+  ngonInit() {
+    this.filteredList = [...this.housingList];
+  }
+
+}
